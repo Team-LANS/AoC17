fbc16717b29561473afd524be62e1229b6a128de
Lena Schnedlitz <lena.schnedlitz@gmx.at>
2017-12-02 22:48:30 +0100

    Remove long headlines

--- 02/day02.py
+++ 02/day02.py
@@ -1,11 +1,12 @@
 m = [list(map(int, row.split())) for row in open('input.txt', 'r').read().split("\n")]
 
-# PART 1 ######################################################################
+
+# PART 1 #
 
 print(sum([max(row) - min(row) for row in m]))
 
 
-# PART 2 ######################################################################
+# PART 2 #
 
 def div(l):
     for i, v in enumerate(l):

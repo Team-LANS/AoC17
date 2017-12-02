6f739d4a79411021588698fc2d9aeb8f8fe11918
Lena Schnedlitz <lena.schnedlitz@gmx.at>
2017-12-02 22:32:10 +0100

    Add part 2 of day 02

--- 02/day02.py
+++ 02/day02.py
@@ -1,5 +1,19 @@
+m = [list(map(int, row.split())) for row in
+     open('input.txt', 'r').read().split("\n")]
+
 # PART 1 ######################################################################
 
-m = [list(map(int, row.split())) for row in open('input.txt', 'r').read().split("\n")]
-
 print(sum([max(row) - min(row) for row in m]))
+
+
+# PART 2 ######################################################################
+
+def div(l):
+    for i, v in enumerate(l):
+        for j, w in enumerate(l):
+            if i != j:
+                if not v % w:
+                    return v // w
+
+
+print(sum([div(row) for row in m]))

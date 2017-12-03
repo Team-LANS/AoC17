0089cacb41f11e6b7b2f844eb4e050a1d312619a
Lena Schnedlitz <lena.schnedlitz@gmx.at>
2017-12-03 20:19:11 +0100

    One-line-ify part 2 of day 02

--- 02/day02.py
+++ 02/day02.py
@@ -1,5 +1,5 @@
-m = [list(map(int, row.split())) for row in open('input.txt', 'r').read().split("\n")]
-
+m = [list(map(int, row.split())) for row in
+     open('input.txt', 'r').read().split("\n")]
 
 # PART 1 #
 
@@ -8,12 +8,4 @@ print(sum([max(row) - min(row) for row in m]))
 
 # PART 2 #
 
-def div(l):
-    for i, v in enumerate(l):
-        for j, w in enumerate(l):
-            if i != j:
-                if not v % w:
-                    return v // w
-
-
-print(sum([div(row) for row in m]))
+print(sum([sum([sum([v // w for w in row if w < v and not v % w]) for v in row]) for row in m]))
